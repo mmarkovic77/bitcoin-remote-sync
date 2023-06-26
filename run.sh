8a1b77debcf4a1cce9c8115949acba95623d63c2
bitcoin-ops <dev@bitcoin-ops>
2023-06-26 13:19:56 +0200

    add support for env vars

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
+set -e
 python3 -m venv venv
 . venv/bin/activate
 pip install "pip>=21.3.1"
 pip install -r requirements.txt
-ansible-playbook main.yml
+ansible-playbook main.yml -e "CLOUD_PROVIDER=$BRS_CLOUD_PROVIDER" -e "CLOUD_API_KEY=$BRS_CLOUD_API_KEY"
