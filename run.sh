b75f41c9e098179af4fdfbe4484c48ff6dc2f873
bitcoin-ops <dev@bitcoin-ops>
2023-06-26 13:25:35 +0200

    remove extra vars

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ python3 -m venv venv
 . venv/bin/activate
 pip install "pip>=21.3.1"
 pip install -r requirements.txt
-ansible-playbook main.yml -e "CLOUD_PROVIDER=$BRS_CLOUD_PROVIDER" -e "CLOUD_API_KEY=$BRS_CLOUD_API_KEY"
+ansible-playbook main.yml
